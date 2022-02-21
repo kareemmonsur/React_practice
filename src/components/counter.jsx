b2461fe5fd751a7e358ab99eb3adeefe4e99f16b
kareemmonsur <monsurkareem@gmail.com>
2022-02-21 05:10:27 +0100

    updating state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,13 +2,12 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
     state = {
-        count: 0,
-        tags: ['tag1', 'tag2', 'tag 3'] 
+        count: 0
 
     };
 
     handleIncrement =() => {
-        console.log("increment clicked", this)
+        this.setState({count: this.state.count + 1})
 
     }
  
@@ -18,9 +17,7 @@ class Counter extends Component {
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
-                <ul>
-                    {this.state.tags.map(tag => <li key={tag}>{tag} </li>)}
-                </ul>
+                
             </div>
         );
     }
